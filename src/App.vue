2ea58a79738a4aca18c155bfd5ee9103dc42c6f3
Emerald <eugenia.polishchuk@gmail.com>
2024-01-16 13:26:27 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,22 @@ onMounted(() => {
   window.OneSignalDeferred = window.OneSignalDeferred || []
 
   window.OneSignalDeferred.push((OneSignal: any) => {
+
+    // OneSignal.SERVICE_WORKER_UPDATER_PATH = '/PushNotifications/OneSignalSDKUpdaterWorker.js';
+    // OneSignal.SERVICE_WORKER_PATH = '/PushNotifications/OneSignalSDKWorker.js';
     
+    console.log('OneSignal', OneSignal)
     OneSignal.init({
-      appId: "a72475a8-840c-4629-97e3-28912437df92",
-      safari_web_id: "web.onesignal.auto.2e21fe47-8329-4413-bae9-ecef4da3342d",
+      // appId: "a72475a8-840c-4629-97e3-28912437df92",
+      // safari_web_id: "web.onesignal.auto.2e21fe47-8329-4413-bae9-ecef4da3342d",
+      appId: "306a5c51-b630-4195-9fb8-71d0b22637ce",
+      safari_web_id: "web.onesignal.auto.44e66786-7e94-4ade-8822-3a1650cda83f",
       notifyButton: {
         enable: true,
       },
+      // serviceWorkerParam: { scope: "/PushNotifications/" },
+      serviceWorkerParam: { scope: "/" },
+      serviceWorkerPath: "PushNotifications/OneSignalSDKWorker.js",
     })
   })
 
