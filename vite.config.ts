f51dc223d27908568d01a22e2ec832aead0415c8
Emerald <eugenia.polishchuk@gmail.com>
2024-01-16 14:11:43 +0200

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
-// import { readFileSync } from 'fs'
 import vue from '@vitejs/plugin-vue'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// const sslCert = readFileSync('./cert.pem')
-// const sslKey = readFileSync('./key.pem')
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'PushNotifications',
-  // server: {
-  //   https: {
-  //     key: sslKey,
-  //     cert: sslCert,
-  //   },
-  // },
   plugins: [
     vue(),
     viteStaticCopy({
