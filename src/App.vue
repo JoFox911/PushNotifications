0ea2cea3b62c6943f8fee2288b6ed043a0624dfa
Emerald <eugenia.polishchuk@gmail.com>
2024-01-21 18:16:14 +0200

    wip

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,54 @@
 <template>
+  <button class="refresh" @click="checkNotificationsStatus">
+
+    <svg fill="#ffffff" height="16px%" width="16px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 489.645 489.645" xml:space="preserve" stroke="#ffffff">
+      <g id="SVGRepo_bgCarrier" stroke-width="0" />
+      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" />
+      <g id="SVGRepo_iconCarrier">
+        <g>
+          <path
+            d="M460.656,132.911c-58.7-122.1-212.2-166.5-331.8-104.1c-9.4,5.2-13.5,16.6-8.3,27c5.2,9.4,16.6,13.5,27,8.3 c99.9-52,227.4-14.9,276.7,86.3c65.4,134.3-19,236.7-87.4,274.6c-93.1,51.7-211.2,17.4-267.6-70.7l69.3,14.5 c10.4,2.1,21.8-4.2,23.9-15.6c2.1-10.4-4.2-21.8-15.6-23.9l-122.8-25c-20.6-2-25,16.6-23.9,22.9l15.6,123.8 c1,10.4,9.4,17.7,19.8,17.7c12.8,0,20.8-12.5,19.8-23.9l-6-50.5c57.4,70.8,170.3,131.2,307.4,68.2 C414.856,432.511,548.256,314.811,460.656,132.911z" />
+        </g>
+      </g>
+    </svg>
+  </button>
+
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    <img src="/onesignal.svg" class="logo" alt="onesignal logo" />
   </div>
 
   <div>
-    Hi there! 14_57
+    <h2>Hi there!</h2>
+
+    <div class="test">
+
+      <span>is browser supports web push? {{ isSupported }} </span>
+
+      <span>is notifications permission prompted? {{ isPermissionPrompted }}</span>
+
+      <span>is subscribed? {{ isSubscribedToNotifications }}</span>
+
+      <button v-if="isSubscribedToNotifications" @click="unsubscribe">
+        Unsubscribe
+      </button>
+
+      <button v-if="!isSubscribedToNotifications" @click="subscribe">
+        Subscribe
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useOneSignal } from '@onesignal/onesignal-vue3'
-import { onBeforeUnmount, onMounted } from 'vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 const OneSignal = useOneSignal()
 
+const isSupported = ref(false)
+const isPermissionPrompted = ref(false)
+const isSubscribedToNotifications = ref(false)
 
 console.log("OneSignal obj", OneSignal)
 
@@ -27,45 +58,51 @@ OneSignal.Debug.setLogLevel('info')
 
 // samples
 function permissionChangeListener(permission: any) {
-  console.log(`permission changed:`, permission);
+  console.log(`permission changed:`, permission)
   if (permission) {
-    console.log(`permission accepted!`);
+    console.log(`permission accepted!`)
   }
 }
 
 function clickEventListener(event: any) {
-  console.log(`click event: ${event}`);
+  console.log(`click event: ${event}`)
 }
 
 function notificationDismissedListener(event: any) {
-  console.log(`dismiss event: ${event}`);
+  console.log(`dismiss event: ${event}`)
 }
 
-async function checkPermissionStatus() {
-  // const permission = await OneSignal.Notifications.permission
-
-  // // debugger
-  // console.log('OneSignal.Notifications.permission', permission)
-  // if (permission) {
-  //   OneSignal.Notifications.requestPermission();
-  // } else {
-  //   window.alert('notifications disabled, please allow')
-  // }
+async function checkNotificationsStatus() {
+  isPermissionPrompted.value = await OneSignal.Notifications.permission
+  isSupported.value = OneSignal.Notifications.isPushSupported()
+  isSubscribedToNotifications.value = OneSignal.User.PushSubscription.optedIn ?? false
 }
 
+
+async function unsubscribe() {
+  await OneSignal.User.PushSubscription.optOut()
+  checkNotificationsStatus()
+}
+
+async function subscribe() {
+  await OneSignal.User.PushSubscription.optIn()
+  checkNotificationsStatus()
+}
 
 onMounted(() => {
-  OneSignal.Notifications.addEventListener("permissionChange", permissionChangeListener);
-  OneSignal.Notifications.addEventListener("click", clickEventListener);
-  OneSignal.Notifications.addEventListener("dismiss", notificationDismissedListener);
+  OneSignal.Notifications.addEventListener("permissionChange", permissionChangeListener)
+  OneSignal.Notifications.addEventListener("click", clickEventListener)
+  OneSignal.Notifications.addEventListener("dismiss", notificationDismissedListener)
 
-  checkPermissionStatus()
+  setTimeout(() => {
+    checkNotificationsStatus()
+  }, 1000)
 })
 
 onBeforeUnmount(() => {
-  OneSignal.Notifications.removeEventListener("permissionChange", permissionChangeListener);
-  OneSignal.Notifications.removeEventListener("click", clickEventListener);
-  OneSignal.Notifications.removeEventListener("dismiss", notificationDismissedListener);
+  OneSignal.Notifications.removeEventListener("permissionChange", permissionChangeListener)
+  OneSignal.Notifications.removeEventListener("click", clickEventListener)
+  OneSignal.Notifications.removeEventListener("dismiss", notificationDismissedListener)
 })
 
 </script>
@@ -77,10 +114,23 @@ onBeforeUnmount(() => {
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #f03d25aa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-</style>
+
+.test {
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+}
+
+.refresh {
+  position: absolute;
+  top: 16px;
+  right: 16px;
+}</style>
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,5 @@ import App from './App.vue'
 
 createApp(App).use(OneSignalVuePlugin, {
     // appId: "a72475a8-840c-4629-97e3-28912437df92",
-    appId: '306a5c51-b630-4195-9fb8-71d0b22637ce',
-    notifyButton: {
-        enable: true,
-    }
+    appId: '306a5c51-b630-4195-9fb8-71d0b22637ce'
 }).mount('#app')
